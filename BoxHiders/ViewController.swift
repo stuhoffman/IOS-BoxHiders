9cd79635a6d15ffc590d8922ce69ef254774703b
Stuart Hoffman <stu@hoffmanswebsite.com>
2016-02-15 16:49:13 -0800

    Changed buttons to toggles for show/hide

--- BoxHiders/ViewController.swift
+++ BoxHiders/ViewController.swift
@@ -10,17 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var BlueButton: UIButton!
+    @IBOutlet var RedButton: UIButton!
     @IBOutlet var RedBox: UIImageView!
     @IBOutlet var BlueBox: UIImageView!
     @IBAction func hideRedBox(sender: AnyObject) {
-        RedBox.hidden = true
+        if (RedBox.hidden)
+        {
+            RedBox.hidden = false
+            RedButton.setTitle("Hide Red", forState: UIControlState.Normal)
+        }
+        else
+        {
+           RedBox.hidden = true
+           RedButton.setTitle("Show Red", forState: UIControlState.Normal)
+        }
     }
     @IBAction func hideBlueBox(sender: AnyObject) {
-        BlueBox.hidden = true
+        if (BlueBox.hidden)
+        {
+            BlueBox.hidden = false
+            BlueButton.setTitle("Hide Blue", forState: UIControlState.Normal)
+        }
+        else
+        {
+            BlueBox.hidden = true
+            BlueButton.setTitle("Show Blue", forState: UIControlState.Normal)
+        }
     }
     @IBAction func showAllBoxes(sender: AnyObject) {
-        RedBox.hidden = false
-        BlueBox.hidden = false
+        resetButtons()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +51,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func resetButtons(){
+        RedButton.setTitle("Hide Red", forState: UIControlState.Normal)
+        BlueButton.setTitle("Hide Blue", forState: UIControlState.Normal)
+        RedBox.hidden = false
+        BlueBox.hidden = false
+        
+    }
 
 }
 
